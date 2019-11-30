7786f42a5c79f48acd2d8326a4838bbf1fa50149
Lee <1981671849@qq.com>
2019-11-30 11:56:55 +0800

    2019-11-30

--- 7-e.c
+++ 7-e.c
@@ -1,29 +1,43 @@
 #include<stdio.h>
 int main()
 {
-	int a, b,tst,prt[10000],c,ery[5],i_=0,iii=0;//a，b是临界值，prt输出值，c位数，ery每一位数，i_第几位
-	scanf("%d %d", &a, &b);
-	for (int i = a; i <= b; i++)
+	int a, b, i,temp,iii=0,j,n=0;//a b临界值，i是测试区间的每个数，temp搓每一位数临时用，iii记录测试数的位数，hw[n]
+	int hw[2000] = { 0 }, ery[6] = { 0 }, zs[2000] = { 0 };
+	scanf("%d %d", &a, &b);//输入边界
+	for (i = a; i <= b; i++)//查回文数，存到hw
 	{
-		if (i - 10 < 0) c = 1;
-		else if (i - 100 < 0) c = 2;
-		else if (i - 1000 < 0) c = 3;
-		else if (i - 10000 < 0) c = 4;
-		else if (i - 100000 < 0) c = 5;
-		while (i_<c)
+		int k=0;
+		temp = i;
+		for (int ii = 0; ii <6; ii++)//把数的每一位搓到ery
 		{
-			ery[i_] = i % 10;
-			i = i / 10;
-			i_++;
+			ery[ii] = temp % 10;
+			if (temp < 10)break;
+			temp = temp / 10;
+			iii++;
 		}
-		for(int ii = 0; ii < c / 2; ii++)
+		for (j = 0; j < iii +1/ 2; j++)
 		{
-			if (ery[ii] == ery[c - 1 - ii])
-			{
-				prt[iii] = i;
-				iii++;
-			}
+			if (ery[j] == ery[iii - j]) k++;//如果相同的数字对数=1/2位数
 		}
+		if (k == iii+1 / 2)
+		{
+			hw[n] = i;
+			n++;
+		}
+		iii = 0;
 	}
-	printf("&d", );
+	//for (j = 0; j < 2000; j++)//测试回文数用
+	//{
+	//	if (hw[j] == 0) break;
+	//	printf("%d\n", hw[j]);
+	//}
+	for (j = 0; j < 2000; j++)
+	{
+		int q = 2;
+		for (int m = 2; m < hw[j]/2; m++)
+		{
+			if (hw[j] % m != 0) q++;
+		}
+		if (q == hw[j] / 2) printf("%d\n", hw[j]);
+	}
 }
