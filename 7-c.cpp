0715b71ba06ced723be53b9f190aa23879d9ac14
Lee <1981671849@qq.com>
2019-12-01 15:30:42 +0800

    2019-12-1

--- 7-c.cpp
+++ 7-c.cpp
@@ -7,8 +7,6 @@ int main()
 	for (i = 0; i < grp; i++) scanf("%s", &ch[i]);
 	for (i = 0; i < grp; i++) scanf("%d", &grp_[i]);
 	for (i = 0; i < grp - 1; i++)
-	
 		for (ii = 0; ii < grp; ii++) if (grp_[ii] == i) printf("%s ", ch[ii]);
-	
 	for (i = 0; i < grp; i++) if (grp_[i] == grp - 1)printf("%s\n", ch[i]);
 }
