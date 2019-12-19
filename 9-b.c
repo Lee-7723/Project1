ea61b28bbccae218dbea7f7bdc6fad695d2909f3
Lee <1981671849@qq.com>
2019-12-19 10:49:48 +0800

    2019-12-19 modified 9-c,pass

--- 9-b.c
+++ 9-b.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 int main()
 {
-	int soldier, tool,tst=0;
-	long long toen[10000] = {0};
+	int soldier, tool;
+	long long toen[10000] = { 0 }, tstmin = 1, tst = 1, tstmax = 2000000;
 	scanf("%d %d", &tool, &soldier);
 	for (int i = 0; i < tool; i++)scanf("%lld", &toen[i]);
-	while (tst>-1)
+	while (tst > -1)
 	{
+		tst = (tstmin + tstmax) / 2;
 		int sum = 0;
-		tst++;
 		for (int i = 0; i < tool; i++)sum = sum + toen[i] / tst;
-		if (sum < soldier)break;
+		
+		if (sum >= soldier)//tst太小了，需要增大
+			tstmin = tst;
+		else if (sum < soldier)//tst太大了，需要缩小
+			tstmax = tst;
+		if (tstmin == tstmax - 1/*sum < soldier*/)break;
+
 	}
-	printf("%d", tst - 1);
+	printf("%lld", tstmin);
 }
